560bff41222296141d2a1cc204e104651ae5ad8a
ivanquintans <ivanqg21@gmail.com>
2024-03-08 16:40:34 +0100

    chore: inicializado TS.h

---
+++ TS.h
@@ -0,0 +1,15 @@
+#ifndef TABLASIMBOLOS_H
+#define TABLASIMBOLOS_H
+
+#include "definiciones.h"
+
+// funcion que inicializa la tabla de simbolos
+void inicializar();
+
+// funcion que comprueba que esta en la tabla de simbolos
+bool estaEnTS();
+
+// funciona que libera la memoria de manera adeduaca al finalizar la tabla de simbolos
+void finalizarTS();
+
+#endif
